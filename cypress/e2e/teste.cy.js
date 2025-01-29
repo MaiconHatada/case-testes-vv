1715b2c6da27e5c37922be70ce0e841318870f74
Maicon Hatada <maicon.hatada17@gmail.com git config --global user.name Maicon>
2025-01-28 21:42:42 -0300

    Teste GitHub Actions v4

--- cypress/e2e/teste.cy.js
+++ cypress/e2e/teste.cy.js
@@ -17,7 +17,7 @@ describe('local dos testes', () => {
     it('Alterando a cor do produto selecionado', () => {
         cy.escolhaDaCorEColocandoNoCarrinho() // produto selecionado HP PAVILION 15Z TOUCH LAPTOP
         cy.get('tr.ng-scope > :nth-child(2) > .roboto-regular').contains('HP PAVILION 15Z TOUCH LAPTOP') // Validando o produto escolhido
-        cy.get('span[title="YELLO"]').should('be.visible') // Validando a cor do produto escolhido
+        cy.get('span[title="YELLOW"]').should('be.visible') // Validando a cor do produto escolhido
 
     })
 })
